e0d2587bb0516a3326f038b91b6180e6425cefb9
Mike Jin <service@amdiscountfurniture.com>
2022-03-09 13:32:26 -0500

    make it better

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static('build'))
+	app.get('*', (req, res) => {
+		req.sendFile(path.resolve(__dirname, 'buikd', 'index.html'));
+	})
+}
+
+app.listen(port, (err) => {
+	if (err) return console.log(err);
+	console.log('Server running on port: ', port);
+})
