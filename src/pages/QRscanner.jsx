a49da6af7127fe4c603aff4dfaec82f08ad71c85
Mike Jin <service@amdiscountfurniture.com>
2022-03-25 16:22:49 -0400

    new

--- src/pages/QRscanner.jsx
+++ src/pages/QRscanner.jsx
@@ -137,7 +137,7 @@ class QRScan extends React.Component {
                     const getProducts = async ()=>{
                         try {
                             const res = await axios.get(
-                                "https://inventory-qr-api.herokuapp.com/api/products/"+result?.text
+                                "https://inventory-qr-api.herokuapp.com/api/products/"+result.text
                                 //"https://inventory-qr-api.herokuapp.com/api/products/find/623a2c1e6f9c8838e7b4189f"
                             );
                             //console.log(res.data);
