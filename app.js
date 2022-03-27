e313b74a2eb284c38e73ef6ebdd6fa97023a5fb6
Mike Jin <service@amdiscountfurniture.com>
2022-03-27 16:25:47 -0400

    new

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const port = process.env.PORT || 5000;
 
 //if (process.env.NODE_ENV === "production") {
 	app.use(express.static('build'))
-	app.get('*', (req, res) => {
+	/*app.get('*', (req, res) => {
 		req.sendFile(path.resolve(__dirname, 'buikd', 'index.html'));
-	})
+	})*/
 //}
+
+app.get('/',function(req, res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
 
 app.listen(port, (err) => {
 	if (err) return console.log(err);
